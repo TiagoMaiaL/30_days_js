9ba4841d13ed4078ee75e6ff4cb687e10609d3fc
Tiago Maia <tiago.maia.lopes@gmail.com>
2017-01-18 01:58:55 -0200

    Adding the code to play the sound.

--- 01_drum-kit/assets/js/drum-kit.js
+++ 01_drum-kit/assets/js/drum-kit.js
@@ -1,34 +1,57 @@
 (function() {
 
+  // TODO: Add the docblocks to this project.
+
   function DrumKit() {
     this.panel = document.querySelector('div.beat-inputs');
   }
 
-  DrumKit.prototype.getCodeButton = function(code) {
-    return this.panel.querySelector('div[key-code="' + code + '"]');
+  DrumKit.prototype.getButton = function(code) {
+    var buttonNode = this.panel.querySelector('div[key-code="' + code + '"]');
+
+    if (buttonNode == null) {
+      throw Error('Button not found.');
+    }
+
+    return buttonNode;
   }
 
-  DrumKit.prototype.getCodeSound = function(code) {
-    return document.querySelector('audio[key-code="' + code + '"]');
+  DrumKit.prototype.getAudio = function(code) {
+    var audioNode = document.querySelector('audio[key-code="' + code + '"]');
+
+    if (audioNode == null) {
+      throw Error('Audio not found.');
+    }
+
+    return audioNode;
   }
 
   DrumKit.prototype.activateButton = function(buttonNode) {
-
+    // TODO: Write this code when the css becomes finished
   }
 
   DrumKit.prototype.deactivateButton = function(buttonNode) {
-
+    // TODO: Write this code when the css becomes finished
   }
 
-  DrumKit.prototype.playSound = function(soundNode) {
-
+  DrumKit.prototype.playAudio = function(code) {
+    try {
+      this.getAudio(code).play();
+    } catch (error) {
+      return;
+    }
   }
 
   var drumKit = new DrumKit();
   var inputHandler = function(event) {
-    // TODO: distinguish between keydown and keyup
-    // TODO: in case of being keyup, play the sound and add the active class to the button
-    // TODO: otherwise, remove the active class.
+    var code = event.keyCode;
+
+    if (event.type == 'keydown') {
+      drumKit.playAudio(code);
+      drumKit.activateButton(code);
+    } else if (event.type == 'keyup') {
+      drumKit.deactivateButton(code);
+    }
   }
 
   window.addEventListener('keydown', inputHandler);
