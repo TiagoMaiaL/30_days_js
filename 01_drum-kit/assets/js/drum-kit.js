700ef9ec225ca73e1940f2b7e1d2917d1d7ecaa6
Tiago Maia <tiago.maia.lopes@gmail.com>
2017-01-17 23:30:00 -0200

    Adding initial interface code for the drum-kit machine.

--- 01_drum-kit/assets/js/drum-kit.js
+++ 01_drum-kit/assets/js/drum-kit.js
@@ -1,3 +1,36 @@
 (function() {
-  // TODO: Create the drum-kit object constructor
+
+  function DrumKit() {
+
+  }
+
+  DrumKit.prototype.handleInputs = function(event) {
+
+  }
+
+  DrumKit.prototype.getCodeButton = function(code) {
+
+  }
+
+  DrumKit.prototype.getCodeSound = function(code) {
+
+  }
+
+  DrumKit.prototype.activateButton = function(buttonNode) {
+
+  }
+
+  DrumKit.prototype.deactivateButton = function(buttonNode) {
+
+  }
+
+  DrumKit.prototype.playSound = function(soundNode) {
+
+  }
+
+  var drumKit = new DrumKit();
+
+  window.addEventListener('keydown', drumKit.handleInputs);
+  window.addEventListener('keyup', drumKit.handleInputs);
+
 })();
